0f7879324655c58634a63b12465a52d7cfaa9931
Marvin <marvin.vandyk@gmx.de>
2022-11-03 20:05:38 +0100

    FTB Modpack Downlader
    Download all latest versions of all Modbacks from FTB

---
+++ FTB Modpacks.py
@@ -0,0 +1,63 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+import time
+import wget
+import os
+
+
+def use_regex(input_text):
+    pattern = re.findall(r"https://api\.modpacks\.ch/public/modpack/[0-9]+/[0-9]+/server/windows", input_text)
+    return pattern
+
+
+path = r"ADD FILEPATH THE FILES SHOULD BE STORED"
+index = 1
+matches = []
+not_downloaded = []
+
+for i in range(130):
+    time.sleep(0.6)
+    url = f'https://www.feed-the-beast.com/modpacks/{index}'
+    print(url)
+
+    html_text = requests.get(url)
+    print(html_text.status_code)
+    if html_text.status_code == 404:
+        index += 1
+        pass
+
+    else:
+        soup = BeautifulSoup(html_text.content, 'html.parser')
+
+        title = [title for title in soup.find_all('title')][0]
+        link = [link.get('href') for link in soup.find_all('a')]
+        for i in link:
+            if use_regex(i):
+                matches.append(i)
+                print(i)
+                try:
+                    version = [version for version in soup.find_all("div", {"class": "Stat_value__qUmap"})][2]
+                    title = title.get_text().replace(':', '')
+
+                    if not os.path.exists(fr"{path}\{title}"):
+                        os.mkdir(fr"{path}\{title}")
+
+                    time.sleep(0.2)
+
+                    if not os.path.exists(fr"{path}\{title}\{version.get_text()}"):
+                        os.mkdir(fr"{path}\{title}\{version.get_text()}")
+
+                    time.sleep(0.2)
+                    wget.download(i, fr"{path}\{title}\{version.get_text()}\{title}.exe")
+                except ValueError or FileExistsError:
+                    not_downloaded.append(i)
+                    pass
+        index += 1
+
+print(matches)
+print(f"Not Downloaded: {not_downloaded}")
+
+
+
+
